5e8fda738b4fdf52f48c4503ead7c85a81a192a9
Laks Gamage <agplg2@gmail.com>
2025-06-21 11:23:56 +0530

    connected to the database

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+
+//connecting to the database
+$conn = mysqli_connect('localhost','cyndi','00010234','pizzamate');
+
+
+//checking the connection error
+if(!$conn) {
+    echo 'mysql error' . mysqli_connect_error();
+}
+
+
+
+//selecting all details needed form the table. title, Ingredients, id
+$sql = 'SELECT id, title, email, Ingredients FROM pizza ORDER BY created_at';
+
+
+
+//getting the results.
+$results = mysqli_query($conn, $sql);
+
+$pizzas = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+
+//free results from memory
+mysqli_free_result($results);
+
+//close the application
+mysqli_close($conn);
+
+
+print_r($pizzas);
+
+
+?>
+
+
+
+
 <html>
 
 <head>
